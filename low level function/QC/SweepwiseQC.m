22157873943a68cab9ea350bc09fcda652cd9be1
Michellesjs <mjimene7@uwo.ca>
2021-05-10 11:15:30 -0400

    test

--- low level function/QC/SweepwiseQC.m
+++ low level function/QC/SweepwiseQC.m
@@ -1,4 +1,4 @@
-function [qc] = SweepwiseQC(protocols,k,params,cellID,folder,Metadata, sweepIDcount)
+function [ltRMSE, VPs, stRMSE, SweepQC] = SweepwiseQC(CCSeries, CCStimSeries, SweepQC, sweepNr, params)
 
 %{
 SweepwiseQC
@@ -8,70 +8,81 @@ SweepwiseQC
 - measures difference in resting potential at pre- and post-stimulus
 %}
 
-if string(protocols.name) == "long pulse" 
-vec_pre = double(protocols.V{1,k}(protocols.stimOn(1,k)-(params.LPqc_samplWind/protocols.acquireRes(1,k))-1:protocols.stimOn(1,k)-1));
-vec_post = double(protocols.V{1,k}((protocols.stimOff(1,k)+(params.LPqc_recovTime/protocols.acquireRes(1,k))-1):...
-    protocols.stimOff(1,k)+(params.LPqc_recovTime/protocols.acquireRes(1,k))-1+(params.LPqc_samplWind/protocols.acquireRes(1,k))));
-elseif string(protocols.name) == "short pulse"
-vec_pre = double(protocols.V{1,k}(protocols.stimOn(1,k)-(params.SPqc_samplWind/protocols.acquireRes(1,k))-1:protocols.stimOn(1,k)-1));
-vec_post = double(protocols.V{1,k}((protocols.stimOff(1,k)+(params.SPqc_recovTime/protocols.acquireRes(1,k))-1):...
-    protocols.stimOff(1,k)+(params.SPqc_recovTime/protocols.acquireRes(1,k))-1+(params.SPqc_samplWind/protocols.acquireRes(1,k))));
-elseif string(protocols.name) == "NONAIBS" && any(protocols.V{1,k}) && protocols.stimOn(1,k) > 0
-vec_pre = double(protocols.V{1,k}(protocols.stimOn(1,k)-(params.preAIBS_samplWind/protocols.acquireRes):protocols.stimOn(1,k)));
-vec_post = nan;
-else 
-vec_pre = nan;
-vec_post = nan;
-rmse_pre_st = nan;
-end
+StimOn = find(CCStimSeries.data.load~=0,1,'first');
+StimOff = find(CCStimSeries.data.load~=0,1,'last')+1;
+vec_pre = CCSeries.data.load(1:StimOn);
+vec_post = CCSeries.data.load((end-0.25*CCSeries.starting_time_rate)+1:...
+    length(CCSeries.data.load));
+
 
 %% long-term noise
-qc.restVPre = mean(vec_pre);
-qc.rmse_pre = sqrt(mean((vec_pre - qc.restVPre).^2));
-qc.restVPost = mean(vec_post);
-qc.rmse_post = sqrt(mean((vec_post - qc.restVPost).^2));
+restVPre = mean(vec_pre);
+rmse_pre = sqrt(mean((vec_pre - restVPre).^2));
+restVPost = mean(vec_post);
+rmse_post = sqrt(mean((vec_post - restVPost).^2));
+
+ltRMSE = types.core.FeatureExtraction(...
+    'description', 'long-term RMSE pre and post',...
+'electrodes', '', ...
+'features', [rmse_pre, rmse_post], ...
+'times', [StimOn/CCSeries.starting_time_rate, (length(CCSeries.data.load)/CCSeries.starting_time_rate)-0.5]);
+
+%%
+
+diffV_b_e = abs(restVPre-restVPost); % differnce between end and stim onset
+
+VPs = types.core.FeatureExtraction(...
+    'description', 'Membrane Potential Pre, Post and Delta',...
+'electrodes', '', ...
+'features', [restVPre, restVPost, diffV_b_e], ...
+'times', [StimOn/CCSeries.starting_time_rate, (length(CCSeries.data.load)/CCSeries.starting_time_rate)-0.5, ...
+          (length(CCSeries.data.load)/CCSeries.starting_time_rate)-0.5 - StimOn/CCSeries.starting_time_rate
+                     ]);
 
 %% short-term noise
-stWin = round(1.5/protocols.acquireRes(1,1));
+stWin = round(1.5*(CCSeries.starting_time_rate/1000));
 winCount = 1;
 for i = 1:round(stWin/2):length(vec_pre)-stWin
     yhat = mean(vec_pre(1,i:i+stWin));
     rmse_pre_st(winCount) = sqrt(mean((vec_pre(1,i:i+stWin) - yhat).^2));
 end
+rmse_pre_st = mean(rmse_pre_st);
 
-qc.rmse_pre_st = mean(rmse_pre_st);
-%%
-if ~isnan(vec_post) 
- winCount = 1;
- for i = 1:round(stWin/2):length(vec_pre)-stWin
+winCount = 1;
+ for i = 1:round(stWin/2):length(vec_post)-stWin
     yhat = mean(vec_post(1,i:i+stWin));
     rmse_post_st(winCount) = sqrt(mean((vec_post(1,i:i+stWin) - yhat).^2));
     winCount = winCount+1;  
  end   
-qc.rmse_post_st = mean(rmse_post_st);
-qc.diffV_b_e = abs(qc.restVPre-qc.restVPost); % differnce between end and stim onset
-else
-qc.rmse_post_st = NaN;   
-qc.diffV_b_e = NaN;
-end
+ 
+rmse_post_st = mean(rmse_post_st);
+
+stRMSE = types.core.FeatureExtraction(...
+    'description', 'short-term RMSE pre and post',...
+'electrodes', '', ...
+'features', [rmse_pre_st, rmse_post_st], ...
+'times', [StimOn/CCSeries.starting_time_rate, (length(CCSeries.data.load)/CCSeries.starting_time_rate)-0.5]);
+
 
 %% Creating the logic vector
 
-if ~isfield(Metadata,'membrane_resistance')
-   membrane_resistance = nan;
+logicVec = [rmse_pre_st > params.RMSEst, ...
+    rmse_post_st > params.RMSEst, ...
+    rmse_pre > params.RMSElt, ...
+    rmse_post > params.RMSElt, ...
+    diffV_b_e > params.maxDiffBwBeginEnd, ...
+    restVPre > params.maximumRestingPot 
+    ];
+    
+
+if sum(logicVec) > 0
+SweepQC.features(sweepNr, 1) = 0;
+b=num2str(find(logicVec));
+idx=strfind(b,' ');
+b(idx)=[];
+SweepQC.features(sweepNr, 1) = str2double(b);
 end
 
-qc.logicVec = [qc.rmse_pre_st > params.RMSEst, ...
-    qc.rmse_post_st > params.RMSEst, ...
-    qc.rmse_pre > params.RMSElt, ...
-    qc.rmse_post > params.RMSElt, ...
-    qc.diffV_b_e > params.maxDiffBwBeginEnd, ...
-    qc.restVPre > params.maximumRestingPot, ...
-    abs(protocols.holding_current(k)) >  params.holdingI, ...
-    protocols.bridge_balance(k) >  params.bridge_balance, ...
-    protocols.bridge_balance(k)> ...
-    params.factorRelaRa*membrane_resistance];
-    
 if params.plot_all == 1
     figure('Position',[50 50 250 250]); set(gcf,'color','w');
     hold on
