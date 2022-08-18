2e9b34c739a8bb98eda288616a85fc5b9bf81427
mfeyerab <mfeyerab@uwo.ca>
2022-08-18 00:30:00 -0400

    hotfix for previous commit

--- SweepRemovalTP.m
+++ SweepRemovalTP.m
@@ -21,49 +21,41 @@ for n = 1:length(cellList)                                                 % for
  if ~isfile(fullfile(path,'inputTabsTP',[cellID,'_TP.csv']))
    nwb = nwbRead(fullfile(cellList(n).folder,cellList(n).name));         % load nwb file
    %% Improve readability by creating additonal variables with shorter names
-   ICEtab = nwb.general_intracellular_ephys_intracellular_recordings;        % assigning IntracellularRecordinsTable to new variable for readability of subsequent code
-   RespTbl = ICEtab.responses.response.data.load;                            % loading all sweep response from IntracellularRecordingsTable
+   ICEtab = nwb.general_intracellular_ephys_intracellular_recordings;      % assigning IntracellularRecordinsTable to new variable for readability of subsequent code
+   RespTbl = ICEtab.responses.response.data.load;                          % loading all sweep response from IntracellularRecordingsTable
    %% Initalizing cell variables
    Idx = find(~contains(string(...
          ICEtab.dynamictable.values{1}.vectordata.values{1}.data.load),...
                    PS.SkipTags));
    QC.pass = table(); 
    temp = regexp({RespTbl.timeseries.path},'\w*','match');
-   QC.pass.SweepID = cellfun(@(z)z(2),temp(Idx))';
+   QC.pass.SweepID = cellfun(@(z)z(2),temp)';
    QC.pass.Protocol = ICEtab.dynamictable.values{...
-                                   1}.vectordata.values{1}.data.load(Idx)';
-   QC.testpulse = zeros(length(Idx),(PS.preTP+PS.TPtrace)*...
-         nwb.acquisition.values{Idx(1)}.starting_time_rate);    
-   QC.pass.TP =  repmat({NaN},length(Idx),1);
-   if contains(cellID, "JR")
-        QC.pass.TP = ones(height(QC.pass),1); 
-   else
-     for i = 1:length(Idx)
-      CurrentPath = table2array(RespTbl(Idx(i),3)).path;                        % get path to sweep within nwb file  
-      PreStimData = nwb.resolve(ICEtab.stimuli.stimulus.data.load(...
-           ).timeseries(Idx(i)).path).data.load(1:RespTbl{Idx(i),1}-OnsetBuffer); 
-      temp = regexp(CurrentPath,'\w*','match'); 
-      QC.pass.SweepID(Idx(i),:) = temp(length(temp));
-      QC.pass.Protocol(Idx(i),:) = ICEtab.dynamictable.values{...
-                                1}.vectordata.values{1}.data.load(i); 
-                                      CCSers = nwb.resolve(CurrentPath);                                       % load the CurrentClampSeries of the respective sweep   
-      PS.SwDat.CurrentName = CurrentPath;
-      if range(PreStimData)<15
-         disp([CurrentPath, 'has no test pulse'])
-         QC.pass.TP = ones(height(QC.pass),1);
-      else
-        [~, QC.testpulse(Idx(i),:)] = getTestPulse(PS,CCSers, PreStimData);         
-      end
-     end
-     tempTPvec = QC.testpulse; save('tempTPvec', 'tempTPvec'); TestPulseComparision
-     prompt = ' from which sweep onwards are test pulses unacceptable? Enter 0 if no manual removal necessary';
-     x = input(['For ', cellID, prompt]);
-     if x == 0
-      QC.pass.TP = ones(height(QC.pass),1);
+                                   1}.vectordata.values{1}.data.load;
+   SmplRt = nwb.resolve(RespTbl.timeseries(Idx(1)).path).starting_time_rate;
+   QC.testpulse = zeros(length(Idx),(PS.preTP+PS.TPtrace)*SmplRt);    
+   QC.pass.TP =  repmat({NaN},ICEtab.id.data.dims,1);
+   for i = 1:length(Idx)
+     CurrentPath = table2array(RespTbl(Idx(i),3)).path;                    % get path to sweep within nwb file  
+     PreStimData = nwb.resolve(ICEtab.stimuli.stimulus.data.load(...
+       ).timeseries(Idx(i)).path).data.load(1:RespTbl{Idx(i),1}-OnsetBuffer); 
+     CCSers = nwb.resolve(CurrentPath);                                    % load the CurrentClampSeries of the respective sweep   
+     PS.SwDat.CurrentName = CurrentPath;
+     if range(PreStimData)<15
+       disp([CurrentPath, 'has no test pulse'])
+       QC.pass.TP = ones(height(QC.pass),1);
      else
-      QC.pass.TP = [ones(x-1,1); zeros(height(QC.pass)-x+1,1)];
+       [~, QC.testpulse(Idx(i),:)] = getTestPulse(PS,CCSers, PreStimData);         
      end
-  end
+   end
+   tempTPvec = QC.testpulse; save('tempTPvec', 'tempTPvec'); TestPulseComparision
+   prompt = ' from which sweep onwards are test pulses unacceptable? Enter 0 if no manual removal necessary';
+   x = input(['For ', cellID, prompt]);
+   if x == 0
+     QC.pass.TP(Idx) = num2cell(ones(length(Idx),1));
+   else
+     QC.pass.TP(Idx) = num2cell([ones(x-1,1); zeros(length(Idx)-x+1,1)]);
+   end
   writetable(QC.pass, fullfile(path,'inputTabsTP',[cellID,'_TP.csv']))
   disp(['exporting ', cellID])
   close all
